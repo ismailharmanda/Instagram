1611ea2891ddd817ae3cba50599584951b600461
İsmail Harmanda <iharmanda@hotmail.com>
2023-11-06 17:52:15 +0300

    integrate pods, install firebase

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by ismail harmanda on 6.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
