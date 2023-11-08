065933383a5331e6febd5817287e8eefba121787
İsmail Harmanda <iharmanda@hotmail.com>
2023-11-08 09:16:32 +0300

    add signup feature

--- Instagram/ViewController.swift
+++ Instagram/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
     
@@ -26,6 +27,39 @@ class ViewController: UIViewController {
     
     
     @IBAction func onPressSignUp(_ sender: UIButton) {
+        
+       let auth = Auth.auth()
+        
+        if (emailText.text == "" || passwordTextField.text == ""){
+            
+            makeAlert(title: "Error", message: "email or password can not be empty")
+            
+            return
+        }
+
+        
+        auth.createUser(withEmail: emailText.text!, password: passwordTextField.text!) { result, error in
+            if error != nil {
+                self.makeAlert(title: "Error", message: error?.localizedDescription ?? "Error")
+            } else{
+                self.performSegue(withIdentifier: "toTabBarVC", sender: nil)
+            }
+        }
+        
+    }
+    
+    func makeAlert(title:String, message:String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+    
+    
+        let confirmAction = UIAlertAction(title: "Confirm", style: .default) { action in
+            alert.dismiss(animated: true)
+        }
+    
+        alert.addAction(confirmAction)
+        
+        present(alert, animated: true)
     }
     
 }
