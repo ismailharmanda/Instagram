e0a476129aab1cf109f2611cf320756abc74bf0a
İsmail Harmanda <iharmanda@hotmail.com>
2023-11-09 23:44:41 +0300

    add upload feature

--- Instagram/UploadViewController.swift
+++ Instagram/UploadViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
-class UploadViewController: UIViewController {
+class UploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var textView: UITextField!
+    
+    @IBOutlet weak var saveButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(onPressImage))
+        imageView.addGestureRecognizer(tapGestureRecognizer)
+        imageView.isUserInteractionEnabled = true
     }
+    
+    @objc func onPressImage(){
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            imageView.image = info[.originalImage] as? UIImage
+            self.saveButton.isEnabled = true
+            self.dismiss(animated: true)
+        }
     
 
     /*
@@ -26,4 +49,6 @@ class UploadViewController: UIViewController {
     }
     */
 
+    @IBAction func onPressSave(_ sender: UIButton) {
+    }
 }
