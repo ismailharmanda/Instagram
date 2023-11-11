32f8e4de589d0c53068bf060652d443e42cb78ad
İsmail Harmanda <iharmanda@hotmail.com>
2023-11-12 01:43:31 +0300

    create database and write image data

--- Instagram/UploadViewController.swift
+++ Instagram/UploadViewController.swift
@@ -6,7 +6,9 @@
 //
 
 import UIKit
+import FirebaseAuth
 import FirebaseStorage
+import FirebaseFirestore
 
 class UploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -49,6 +51,16 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate, U
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func makeAlert(titleInput: String, messageInput: String){
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: .alert)
+        
+        let confirmButton = UIAlertAction(title: "OK", style: .default) { action in
+            
+        }
+        alert.addAction(confirmButton)
+        self.present(alert,animated:true)
+    }
 
     @IBAction func onPressSave(_ sender: UIButton) {
         
@@ -64,12 +76,30 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate, U
             let imageReference = mediaFolder.child("\(uuid).jpg")
             imageReference.putData(data) { metaData, error in
                 if error != nil {
-                    print(error?.localizedDescription)
+                    self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error")
                 } else{
                     imageReference.downloadURL { url, error in
                         if error == nil{
                             let imageUrl = url?.absoluteString
-                            print(imageUrl)
+                            
+                            //DATABASE
+                            
+                            let firestoreDatabase = Firestore.firestore()
+                            
+                            var firestoreReference : DocumentReference?
+                            
+                            let firestorePost:[String:Any] = ["imageUrl": imageUrl!, "postedBy":Auth.auth().currentUser?.email, "postComment": self.textView.text, "date":FieldValue.serverTimestamp(), "likes":0]
+                            
+                            firestoreReference = firestoreDatabase.collection("Posts").addDocument(data: firestorePost, completion: { error in
+                                if error != nil {
+                                    self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error!")
+                                } else{
+                                    self.imageView.image = UIImage(systemName: "plus.viewfinder")
+                                    self.textView.text = ""
+                                    self.tabBarController?.selectedIndex = 0
+                                }
+                            })
+                            
                         }       else{
                             print(error)
                         }             }
